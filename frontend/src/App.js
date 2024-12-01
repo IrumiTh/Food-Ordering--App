909384352cce307e1d90f779073e338a131fa0ff
irumi <irumiabeywickrama@gmail.com>
2024-12-01 09:47:57 +0530

    complete backend and frontend for home page

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme } from './Theme/DarkTheme';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <Navbar/>
+      <Home/>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home/topMeel.js
@@ -0,0 +1,59 @@
+export const topMeel=[
+    {
+        image:"https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg",
+        title: "Pizza"
+    },
+    {
+        image:"https://media.istockphoto.com/id/2061716709/photo/grilled-rib-burger.jpg?s=2048x2048&w=is&k=20&c=GysiZtJ85wDZCAXWA9CgqXamsdmhQzsBTUTZhIaFQ3Q=",
+        title: "Burger"
+    },
+    {
+        image:"https://media.istockphoto.com/id/2150377906/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=2048x2048&w=is&k=20&c=fym_KisWaMVp4b2klJI6zJ9aI-MkwExGoHWfxF3ux8A=",
+        title: "Sandwich"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2016/11/20/09/06/bowl-1842294_960_720.jpg",
+        title: "Chips"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2022/01/19/21/11/food-6950998_960_720.jpg",
+        title: "Donate"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2019/05/08/13/49/food-4188662_960_720.jpg",
+        title: "Roll"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2017/03/31/18/02/strawberry-dessert-2191973_960_720.jpg",
+        title: "Strawberry Dessert"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2023/01/09/10/56/meal-7707134_960_720.jpg",
+        title: "Bread"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2018/08/16/22/59/ice-cream-3611698_1280.jpg",
+        title: "Ice cream"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg",
+        title: "Seafood"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2020/03/18/15/49/cupcakes-4944589_960_720.jpg",
+        title: "Cupcake"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2022/05/22/13/36/raspberries-7213407_1280.jpg",
+        title: "Yoghurt"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2016/12/06/18/27/cheese-1887233_1280.jpg",
+        title: "Snacks"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132_960_720.jpg",
+        title: "Meat"
+    },
+
+]
